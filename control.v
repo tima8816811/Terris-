1dbcdc5fca516f1ac2e04de24ffbd03bd9c30853
tima8816811 <104099130+tima8816811@users.noreply.github.com>
2022-05-27 15:43:52 +0800

    Update control.v

--- control.v
+++ control.v
@@ -22,15 +22,15 @@
 
 module control(
 input clk,clr,rotate,left,right,down,start,
-input shift_finish,remove_2_finish,down_comp,move_comp,die,
-output reg  [3:0] opcode,
-output reg  gen_random,keep,renew1,move_down,renew2,remove,stop,move,new,auto_down
+input shift_finish,remove_finish,down_able,move_able,die_true,
+output reg  [3:0] code,
+output reg  gen_random,renew1,move_down,renew2,remove,stop,move,die,auto_down
 );
 //not reg not debounce
 parameter time_f = 26'd25000001;
 reg [25:0] time_cnt;
 reg auto_down_reg;
-    
+reg keep;
   
     
 always @ (posedge clk or posedge clr)
@@ -87,8 +87,8 @@ reg [3:0] state, next_state;
     
     always @ (posedge clk or posedge clr)
     begin
-        if (clr) opcode<=0;
-        else opcode<={right, left, down, rotate};
+        if (clr) code<=0;
+        else code<={right, left, down, rotate};
     end 
        
         
@@ -97,14 +97,14 @@ reg [3:0] state, next_state;
         next_state = S_blank;
         keep = 1;
         gen_random = 0;
-        //opcode = 4'b0000;
+        //code = 4'b0000;
         renew1 = 0;
         move_down = 0;
         renew2 = 0;
         remove = 0;
         stop = 0;
         move = 0;
-        new = 0;
+        die = 0;
         case (state)
         S_blank:
         begin
@@ -121,20 +121,19 @@ reg [3:0] state, next_state;
         S_keep:
         begin
             keep = 0;
-            if (time_cnt == time_f)   
+            if (time_cnt == time_f)  
             begin
                 next_state = S_down;
-        
+         
             end
             else if (down == 1)
             begin
                next_state = S_down;
-
+  
             end
             else if ((left == 1)|| (right == 1)||(rotate == 1))
             begin
                 next_state = S_move;
-
             end
             else
                 next_state = S_keep;
@@ -142,29 +141,23 @@ reg [3:0] state, next_state;
         S_move:    
         begin
             move = 1;
-            if (move_comp)
+            if (move_able)
                 next_state = S_renew1 ;
             else
                 next_state = S_keep;
         end
-        S_renew1 :   
+        S_renew1 : 
         begin
-   
             renew1  = 1;
-   
             next_state = S_keep;
-   
-  
         end
         S_down:
         begin
             move_down = 1;
-            if (down_comp)
+            if (down_able)
                 next_state = S_renew1 ;
             else
-                next_state = S_renew2;
-            
-                
+                next_state = S_renew2;                
         end
         S_renew2:
         begin
@@ -174,15 +167,15 @@ reg [3:0] state, next_state;
         S_remove:
         begin
             remove = 1;
-            if (remove_2_finish)
+            if (remove_finish)
                 next_state = S_new;
             else
                 next_state = S_remove;
         end
         S_new:
         begin
-            new = 1;
-            if (die == 1)
+            die = 1;
+            if (die_true == 1)
                 next_state = S_stop;
             else
                 next_state = S_random;
